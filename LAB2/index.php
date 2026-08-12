736a6b606ce1cd28d5192b31395b2586c9a46ba2
Jashlei James Asis Jumayao <you@example.com>
2026-08-12 10:22:54 +0800

    Added index.php

---
+++ LAB2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Jashlei James A. Jumayao</h1>
+    <h2>About me</h2>
+    <p>I am a student at the University of St. La Salle, pursuing a degree in Computer Science.</p>
+</body>
+</html>
