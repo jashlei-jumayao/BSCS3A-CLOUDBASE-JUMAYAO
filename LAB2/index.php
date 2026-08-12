a32345f6c2e3ac33342997b1ec7fa02fcb8eeb76
Jashlei James Asis Jumayao <you@example.com>
2026-08-12 10:24:32 +0800

    Added index.php

--- LAB2/index.php
+++ LAB2/index.php
@@ -7,8 +7,6 @@
 </head>
 <body>
 
-    <h1>Jashlei James A. Jumayao</h1>
-    <h2>About me</h2>
-    <p>I am a student at the University of St. La Salle, pursuing a degree in Computer Science.</p>
+    <?php echo "<h1> Jashlei James A. Jumayao</h1>"; ?>
 </body>
 </html>
