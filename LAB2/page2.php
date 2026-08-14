14e7c8ab67b2f1f1c13adf50fa0f4f763a100543
Jashlei <jashleijames@gmail.com>
2026-08-14 16:00:06 +0800

    transfered files to main

---
+++ LAB2/page2.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title> <?php echo $title;?> </title>
+            <?php
+    $title = "My Writeups";
+    $headerName = "The Howl Contributions";
+?>
+    </head>
+    <body>
+        <h1><?php echo $headerName ?></h1>
+        <p>These are the writeups I've done since becoming a member for the Howl.</p>
+        
+        <p> <a href="https://www.facebook.com/share/p/1BkQx1BAWb/">https://www.facebook.com/share/p/1BkQx1BAWb/</a></p>
+        <img src=
+        "writeup1.png" alt="writeup1.png" width="400" height="400">
+    <br>
+        <p> <a href="https://www.facebook.com/share/p/1ETAyHZyya/">https://www.facebook.com/share/p/1ETAyHZyya/</a></p>
+        <img src=
+        "writeup2.png" alt="writeup2.png" width="400" height="400">
+    <br>
+    
+
+    <p> <a href="https://www.facebook.com/share/p/1RYj4YdXUb/">https://www.facebook.com/share/p/1RYj4YdXUb/</a></p>
+        <img src=
+        "writeup3.png" alt="writeup3.png" width="400" height="400">
+    <br>
+        <div>Links to other pages</div>
+        <ul>
+            <li><a href="page1.php">Page 1: My Biography</a></li>
+            <li><a href="page3.php">Page 3: Social Media Links</a></li>
+        </ul> 
+
+        
+   
+    </body>  
+</html>
